c28966e895f2dd26de8f5db88d5497832d51b4a4
Deepa1626 <deepanjalis16072006@gmail.com>
2026-09-02 20:04:50 +0530

    Java Built-in stack

---
+++ Builtinstack.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+public class Builtinstack {
+    public static void main(String[] args) {
+        Stack<Integer> stack= new Stack<>();
+        stack.push(30);
+        stack.push(10);
+        stack.push(20);
+        stack.push(50);
+        System.out.println(stack);
+        System.out.println(stack.size());
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
+        
+        
+
+
+    }
+    
+}
